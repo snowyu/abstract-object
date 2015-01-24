c1ffbe12418bbe20dab6e2ec45d1165f2c621702
Riceball LEE <snowyu.lee@gmail.com>
2015-01-24 13:17:50 +0800

    * minor comments

--- src/util/inheritsObject.js
+++ src/util/inheritsObject.js
@@ -2,6 +2,7 @@ var inherits = require('./inherits');
 
 //make sure the aClass.prototype hook to the aObject instance.
 module.exports = function(aObject, aClass) {
+  // ES6: Object.getPrototypeOf / Object.setPrototypeOf
   var vOldProto = aObject.__proto__;
   var result = false;
   if ( vOldProto !== aClass.prototype) {
